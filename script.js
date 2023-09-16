9bd95835e08512cbee5648035f61ab863536ec17
unknown <sharmavimal7467@gmail.com>
2023-09-16 16:44:39 +0530

    submission

--- script.js
+++ script.js
@@ -1,34 +1,88 @@
 const data = [
   { name: "john", age: 24, profession: "developer" },
   { name: "jane", age: 27, profession: "admin" },
-];
-
+]
 // 1. Print Developers
-function printDeveloper() {}
+function printDeveloper() {
+  if(data[0].profession == "developer"){
+    console.log(data[0].name);
+  }
+  else{
+    console.log(data[1].name);
+  }
+}
 
 // 2. Add Data
-function addData() {}
+function addData() {
+  data.splice(2,1,{name:"Viyaan",age:22,profession:"Learner"});
+console.log(data)
+}
 
 // 3. Remove Admins
-function removeAdmin() {}
-
+function removeAdmin() {
+const del = delete data[1].profession;
+  console.log(del);
+  console.log(data[1]);
+}
 // 4. Concatenate Array
-function concatenateArray() {}
+function concatenateArray() {
+  const newArr = {name: "Viyaan", age: 22, profession:"Learner"};
+  const concArr = data.concat(newArr);
+  console.log(concArr);
+}
 
 // 5. Average Age
-function averageAge() {}
+function averageAge() {
+  const aveAge = ((data[0].age+data[1].age)/2)
+  console.log(aveAge);
+}
 
 // 6. Age Check
-function checkAgeAbove25() {}
+function checkAgeAbove25() {
+  for(let i = 0;i<data.length;i++){
+    if(data[i].age>25){
+      console.log("yes,",data[i].name,"age is above 25");
+    }
+  }
+}
 
 // 7. Unique Professions
-function uniqueProfessions() {}
+function uniqueProfessions() {
+  let unique_values = [...new Set(data.map((element)=>element.profession)),];
+  console.log(unique_values);
+}
 
 // 8. Sort by Age
-function sortByAge() {}
+function sortByAge() {
+ data.sort((x,y) => x.age - y.age);
+console.log(data);
+}
 
 // 9. Update Profession
-function updateJohnsProfession() {}
+function updateJohnsProfession() {
+   for(let i = 0;i<data.length;i++){
+    if(data[i].name=="john"){
+       data[i].profession= "manager";
+    }
+    console.log(data[i]);
+    break;
+   }
+}
 
 // 10. Profession Count
-function getTotalProfessions() {}
+function getTotalProfessions() {
+  let ct = 0;
+for(let i = 0;i<data.length;i++){
+  if(data[i].profession=="developer"){
+      ct = ct+1;
+  }  
+}
+console.log("developer are =",ct)
+let ct1=0;
+for(let i = 0;i<data.length;i++){
+  if(data[i].profession=="admin"){
+    ct1 = ct1+1;  
+}
+}
+console.log("admin are =",ct1)
+}
